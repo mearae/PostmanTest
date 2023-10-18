61c8b74ea9faa965b373cffbfbb1c5e81145e4ec
sejinpark2 <>
2023-10-18 10:53:19 +0900

    Update DemoApplication.java
    생성과정

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// spring initializer
+	// 버전 : 2.7.16
+	// java : 11
+	// generate -> demo.zip -> 압축 풀기 -> gradle-wrapper.properties 버전 수정, build.gradle 수정
+
 	// localhost:8080
 	// ID : user
 	// PS : 콘솔 security password: ~~
