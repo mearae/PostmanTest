03651387f884a256dfbf46c848f54851574feef5
sejinpark2 <>
2023-10-18 10:47:57 +0900

    Update DemoApplication.java
    로그인 방법 comment

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+	// localhost:8080
+	// ID : user
+	// PS : 콘솔 security password: ~~
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
