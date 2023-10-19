77ee9225d8ef418a91e9677cb4819fe59810345f
sejinpark2 <>
2023-10-19 10:35:35 +0900

    컬럼 조건 추가
    데이터(email, password)에 패턴 및 조건 추가
    데이터 유효성 검사 추가

--- DataCRUD/src/main/java/com/example/demo/user/UserController.java
+++ DataCRUD/src/main/java/com/example/demo/user/UserController.java
@@ -4,11 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/*
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
-*/
+import javax.validation.Valid;
+
+//    public UserController(UserService userService) {
+//        this.userService = userService;
+//    }
 // ** 생성자를 통해 UserService를 초기화 하는 대신 @RequiredArgsConstructor 추가
 @RequiredArgsConstructor
 @RestController
@@ -16,8 +16,12 @@ public class UserController {
 
     private final UserService userService;
 
+    // @Valid : 받아온 폼의 데이터의 유효성을 검사하는 역할을 수행
+    // - @RequestBody, @ModelAttribute 과 함께 사용
+    // - DTO에서 작성된 @Size, @Pattern, @NotEmpty 등등을 검사
+    // - 필드에 'NOT NULL' 조건이 있거나, 'UNIQUE' 조건이 설정되어 있는 경우도 확인
     @GetMapping("/join")
-    public String login(UserRequest.loginDto loginDto){
+    public String login(@Valid UserRequest.LoginDto loginDto){
 
         String byEmail = userService.findByEmail(loginDto.getEmail());
 
--- DataCRUD/src/main/java/com/example/demo/user/UserRequest.java
+++ DataCRUD/src/main/java/com/example/demo/user/UserRequest.java
@@ -3,14 +3,37 @@ package com.example.demo.user;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class UserRequest {
 
     @Setter
     @Getter
-    public static class loginDto{
+    public static class LoginDto{
 
+        // 데이터가 비어있을 수 없음
+        @NotEmpty
+        @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "유효한 이메일 주소를 입력해주세요.")
         private String email;
+        // ^[A-Za-z0-9+_.-]+: 이메일 주소의 로컬 파트로 영문 대소문자, 숫자, 특수문자(+, _, ., -)가 포함될 수 있음
+        //@[A-Za-z0-9.-]+$: @ 기호 뒤에 도메인 파트로 영문 대소문자, 숫자, 점(.), 하이픈(-)이 포함될 수 있음
 
+        @NotEmpty
+        @Size(min = 8, max = 20, message = "8자 이상 20자 이내로 작성 가능합니다.")
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#$%^&+=])[A-Za-z\\d@#$%^&+=]{8,20}$", message = "영문자, 숫자, 특수문자를 혼합하여 입력해주세요.")
         private String password;
+        // ^(?=.*[A-Za-z]): 적어도 하나의 영문자가 포함되어야 함
+        // (?=.*\\d): 적어도 하나의 숫자가 포함되어야 함
+        // (?=.*[@#$%^&+=]): 적어도 하나의 특수문자(@, #, $, %, ^, &, +, =)가 포함되어야 함
+        // [A-Za-z\\d@#$%^&+=]{8,20}: 영문자, 숫자 및 특수문자만 허용하며, 8~20글자여야 함
+
+        public User toEntity(){
+            return User.builder()
+                    .email(email)
+                    .password(password)
+                    .build();
+        }
     }
 }
