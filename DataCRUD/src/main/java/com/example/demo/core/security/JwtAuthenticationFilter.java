d77ac36e912b44fce184bdbbe910dc04dd274c51
sejinpark2 <>
2023-10-24 11:06:06 +0900

    Update JwtAuthenticationFilter.java
    user의 토큰 검증

--- DataCRUD/src/main/java/com/example/demo/core/security/JwtAuthenticationFilter.java
+++ DataCRUD/src/main/java/com/example/demo/core/security/JwtAuthenticationFilter.java
@@ -1,9 +1,15 @@
 package com.example.demo.core.security;
 
+import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.demo.user.StringArrayConverter;
+import com.example.demo.user.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -11,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Slf4j
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
@@ -18,8 +25,7 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
         super(authenticationManager);
     }
 
-    // **
-
+    // ** Http 요청이 발생할 때마다 호출되는 메서드
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
             throws IOException, ServletException {
@@ -37,12 +43,39 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
 
         try {
             log.debug("토큰 있음.");
+
+            // ** 토큰 검증
             DecodedJWT decodedJWT = JwtTokenProvider.verify(jwt);
-        }
-        catch (AuthenticationException ex) {
 
-        }
+            // ** 사용자 정보 추출
+            int id = decodedJWT.getClaim("id").asInt();
+            String roles = decodedJWT.getClaim("roles").asString();
 
-        chain.doFilter(request, response);
+            // ** 권한 정보를 문자열 리스트로 변환
+            StringArrayConverter stringArrayConverter = new StringArrayConverter();
+            List<String> rolesList = stringArrayConverter.convertToEntityAttribute(roles);
+
+            // ** 추출한 정보로 User를 생성
+            User user = User.builder().id(id).roles(rolesList).build();
+            CustomUserDetails customUserDetails = new CustomUserDetails(user);
+
+            // ** Spring Security / 인증 정보를 관리하는데 사용
+            Authentication authentication = new UsernamePasswordAuthenticationToken(
+                    customUserDetails,
+                    customUserDetails.getPassword(),
+                    customUserDetails.getAuthorities()
+            );
+
+            // ** SecurityContext에 저장
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            log.debug("인증 객체 생성");
+
+        } catch (SignatureVerificationException sve) {
+            log.debug("토큰 검증 실패");
+        } catch (TokenExpiredException tee){
+            log.debug("토큰 검증 실패");
+        } finally {
+            chain.doFilter(request,response);
+        }
     }
 }
